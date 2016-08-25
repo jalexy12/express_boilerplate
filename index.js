754645cd230ea9c30a37b27208944a8c9c4a5db7
Josh <jalexy12@gmail.com>
2016-08-25 13:57:35 -0400

    Creating basic MVC structure

--- index.js
+++ index.js
@@ -1,4 +1,43 @@
 #!/usr/bin/env node
-let fs = require("fs");
+let fs = require("fs")
+let myProgram = require("commander")
+let cmdValue;
 
-console.log("What??")
+myProgram.action(function (cmd, env) { cmdValue = cmd })
+
+myProgram.parse(process.argv)
+
+const directories = [
+	"controllers", 
+	"helpers", 
+	"middlewares",
+	"models",
+	{
+		public: [
+			"js",
+			"css",
+			"html"
+		]
+	},
+	"views"
+]
+
+if (cmdValue) {
+	let mainDir = `${process.cwd()}/${cmdValue}` 
+	fs.mkdirSync(mainDir)
+
+	directories.forEach(function(directory){
+
+		if (typeof(directory) === "string"){
+			fs.mkdirSync(`${mainDir}/${directory}`)
+		} else {
+			fs.mkdirSync(`${mainDir}/public`)
+			
+			directory.public.forEach(function(subDir){
+				fs.mkdirSync(`${mainDir}/public/${subDir}`)
+			})
+		}
+	})
+} else {
+	console.error("Please enter a project name!")
+}
